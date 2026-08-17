31db669ef939472a2f8d3e7b5ea83a218ea69e1a
yuviwork15-collab <yuviwork15-collab@users.noreply.github.com>
2026-08-17 14:15:55 -0700

    Fix profiledef.sh: associative array needs [key]=value syntax (bash)

--- os/profiles/karenos/profiledef.sh
+++ os/profiles/karenos/profiledef.sh
@@ -17,8 +17,8 @@ airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-noappen
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--long' '-8')
 pacman_conf="pacman.conf"
 file_permissions=(
-  ["/etc/shadow"]                       "0:0:400"
-  ["/root"]                             "0:0:750"
-  ["/opt/karen-linux/karen_shell.py"]   "0:0:755"
-  ["/usr/local/bin/karen-bootstrap.sh"] "0:0:755"
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/opt/karen-linux/karen_shell.py"]="0:0:755"
+  ["/usr/local/bin/karen-bootstrap.sh"]="0:0:755"
 )
