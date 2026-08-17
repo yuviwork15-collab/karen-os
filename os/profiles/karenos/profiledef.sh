38d38d2a69910328c47665074defb89e94b889e4
yuviwork15-collab <yuviwork15-collab@users.noreply.github.com>
2026-08-17 14:08:22 -0700

    Fix ISO profile v2: new mkarchiso bootmodes (bios.syslinux, uefi.systemd-boot) + efiboot entries + syslinux.cfg + bootstrap_packages

--- os/profiles/karenos/profiledef.sh
+++ os/profiles/karenos/profiledef.sh
@@ -2,7 +2,7 @@
 # Karen OS - archiso profile definition
 # shellcheck disable=SC2034
 
-build_mode="iso"
+buildmodes=('iso')
 iso_name="karenos"
 iso_label="KARENOS_2026"
 iso_publisher="Karen OS"
@@ -10,13 +10,15 @@ iso_application="Karen OS Live Desktop - low-end optimized Arch Linux"
 iso_version="1.0.0"
 install_dir="karenos"
 arch="x86_64"
-bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.eltorito.esp' 'bios-syslinux.mbr' 'bios-syslinux.eltorito')
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-noappend')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--long' '-8')
 pacman_conf="pacman.conf"
 file_permissions=(
-  "/etc/shadow"                          "0:0:400"
-  "/root"                                "0:0:750"
-  "/opt/karen-linux/karen_shell.py"      "0:0:755"
-  "/usr/local/bin/karen-bootstrap.sh"    "0:0:755"
+  ["/etc/shadow"]                       "0:0:400"
+  ["/root"]                             "0:0:750"
+  ["/opt/karen-linux/karen_shell.py"]   "0:0:755"
+  ["/usr/local/bin/karen-bootstrap.sh"] "0:0:755"
 )
