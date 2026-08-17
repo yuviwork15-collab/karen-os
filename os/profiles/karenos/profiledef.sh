d02fdb5d78055d40d529a57cc5ce71f91556bf72
yuviwork15-collab <yuviwork15-collab@users.noreply.github.com>
2026-08-16 23:16:15 -0700

    Fix ISO profile: profiledef.sh (new mkarchiso format), drop profile.conf, add syslinux for BIOS boot

---
+++ os/profiles/karenos/profiledef.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Karen OS - archiso profile definition
+# shellcheck disable=SC2034
+
+build_mode="iso"
+iso_name="karenos"
+iso_label="KARENOS_2026"
+iso_publisher="Karen OS"
+iso_application="Karen OS Live Desktop - low-end optimized Arch Linux"
+iso_version="1.0.0"
+install_dir="karenos"
+arch="x86_64"
+bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.eltorito.esp' 'bios-syslinux.mbr' 'bios-syslinux.eltorito')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-noappend')
+pacman_conf="pacman.conf"
+file_permissions=(
+  "/etc/shadow"                          "0:0:400"
+  "/root"                                "0:0:750"
+  "/opt/karen-linux/karen_shell.py"      "0:0:755"
+  "/usr/local/bin/karen-bootstrap.sh"    "0:0:755"
+)
